f84e11790fc762186695bc3bafc1779585231fb2
csantos <csantos@shoprunner.com>
2020-03-12 04:46:02 -0500

    e

--- test/storedDataManager.js
+++ test/storedDataManager.js
@@ -74,63 +74,12 @@ function createIDB() {
 
 createIDB();
 
-
-
-postRobot.on('setData', function prSetData(event) {
-    // var daysToExpire = event.data.daysToExpire || 3650; // default to 10yr, like sso cookie
-    // var domain = event.data.domain || '.shoprunner.com'; // default to .shoprunner.com base domain
-    //
-    // if (event.data.name && event.data.value) {
-    //     setCookie(event.data.name, event.data.value, {
-    //         expires: daysToExpire,
-    //         domain: domain,
-    //     });
-    //     setLocalStorage(event.data.name, event.data.value);
-    //
-    //     return {
-    //         value: checkStorageThenCookie(event.data.name),
-    //     };
-    // }
-    //
-    // throw new Error('name and value are required in all setCookie calls');
-});
-
-postRobot.on('getData', function prGetData(event) {
-    // if (event.data.name) {
-    //     // if (event.data.cookieOnly) {
-    //     //     return {
-    //     //         value: getCookie(event.data.name),
-    //     //     };
-    //     // }
-        console.log('[DEBUG] postRobot.getData');
-    //     return {
-    //         value: checkStorageThenCookie(event.data.name),
-    //     };
-    // }
-    //
-    // throw new Error('name is required in all getCookie calls');
-});
-
-postRobot.on('clearData', function prClearData(event) {
-    // if (event.data.name) {
-    //     clearCookie(event.data.name);
-    //     clearLocalStorage(event.data.name);
-    //     return {
-    //         value: checkStorageThenCookie(event.data.name),
-    //     };
-    // }
-    //
-    // throw new Error('name is required in all clearCookie calls');
-});
-
 console.log('stored data manger adding button');
 let $button = document.createElement("BUTTON");
 $button.setAttribute('id', 'mybutton');
 $button.innerHTML = 'create idb';
 document.body.appendChild($button);
 // document.getElementById('mybutton').addEventListener('click', createIDB);
-
-// createIDB();
 
 
 
